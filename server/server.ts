30b91a85065dfb6251d09da64da4ae74d339da3e
Alex Ford <alex.ford@codetunnel.com>
2021-09-21 16:48:09 -0600

    Utilize custom port if specified.

--- server/server.ts
+++ server/server.ts
@@ -3,8 +3,9 @@ import { createServer } from "http";
 import { parse } from "url";
 import next from "next";
 
-console.log(process.env.NODE_ENV);
-const dev = process.env.NODE_ENV !== "production";
+const { NODE_ENV, PORT } = process.env;
+
+const dev = NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
@@ -12,7 +13,7 @@ app.prepare().then(() => {
   createServer((req, res) => {
     const parsedUrl = parse(req.url ?? "", true)
     handle(req, res, parsedUrl);
-  }).listen(3000, () => {
-    console.log('> Ready on http://localhost:3000')
+  }).listen(PORT ?? 3000, () => {
+    console.log(`> Ready on http://localhost:${PORT ?? 3000}`)
   });
 })
