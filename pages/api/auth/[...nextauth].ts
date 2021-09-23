6c7a4759e6dbd708555f10353c7958487a75d6b0
Alex Ford <alex.ford@codetunnel.com>
2021-09-23 12:46:00 -0600

    Added MongoDB adapter for next-auth.

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { NextApiRequest, NextApiResponse } from "next";
 import NextAuth from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "lib/mongodb";
 import config from "config";
 
 const {
@@ -8,16 +11,21 @@ const {
   SESSION_SECRET
 } = config;
 
-export default NextAuth({
-  providers: [
-    DiscordProvider({
-      clientId: DISCORD_CLIENT_ID,
-      clientSecret: DISCORD_CLIENT_SECRET,
-      authorization: "https://discord.com/api/oauth2/authorize?scope=identify+email+guilds.join+guilds&prompt=none"
-    })
-  ],
-  session: {
-    jwt: true
-  },
-  secret: SESSION_SECRET
-});
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  return await NextAuth(req, res, {
+    adapter: MongoDBAdapter({
+      db: (await clientPromise).db("chevcasttv")
+    }),
+    providers: [
+      DiscordProvider({
+        clientId: DISCORD_CLIENT_ID,
+        clientSecret: DISCORD_CLIENT_SECRET,
+        authorization: "https://discord.com/api/oauth2/authorize?scope=identify+email+guilds.join+guilds&prompt=none"
+      })
+    ],
+    session: {
+      jwt: true
+    },
+    secret: SESSION_SECRET
+  });
+}
