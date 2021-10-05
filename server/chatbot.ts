a318ea0fe35d9e00e1485c32a281e0a35b782dc2
Alex Ford <alex.ford@codetunnel.com>
2021-10-04 18:48:06 -0600

    Chatbot foundation complete

--- server/chatbot.ts
+++ server/chatbot.ts
@@ -11,40 +11,39 @@ import {
   entersState,
   joinVoiceChannel,
   NoSubscriberBehavior,
-  StreamType,
   VoiceConnectionStatus
 } from "@discordjs/voice";
 
-const { DISCORD_BOT_TOKEN, GOOGLE_API_KEY } = config;
+const {
+  DISCORD_BOT_TOKEN,
+  GOOGLE_API_KEY,
+} = config;
+
 const log = debug("CHEVCASTTV:CHATBOT");
-const CHANNELS = "Codemanis,HarlequinDollFace,Ember_Stone,rainbowkittencast,SithLordBoris";
-const discordChannelId = "752398756229677171";
-const discordGuildId = "752398755533291571";
 
-let englishVoices: string[] | undefined;
-const userVoice: Record<string, string> = {};
-
-
-export default async function () {
+export default async function (twitchChannels: string[], discordGuildId: string, discordChannelId: string) {
 
   log("Configuring Google TTS client...");
   const ttsClient = new tts.TextToSpeechClient({
     credentials: JSON.parse(GOOGLE_API_KEY)
   });
   const [{ voices }] = await ttsClient.listVoices();
-  englishVoices = voices
-    ?.filter(voice => voice.languageCodes?.[0].match(/^en-(US|UK|AU)/) && typeof voice.name === "string")
+  const availableVoices: string[] = voices!
+    .filter(voice => voice.languageCodes?.[0].match(/^en-/) && typeof voice.name === "string")
     .map(voice => voice.name!);
+  shuffle(availableVoices);
   log("TTS ready for synthesis.");
 
   // *******************************************
 
   log("Configuring Discord client...")
   const discordClient = new DiscordClient({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES] });
-  discordClient.on('ready', () => {
-    log ("Discord client now logged in.");
+  await new Promise((resolve, reject) => {
+    discordClient.once("ready", resolve);
+    discordClient.once("error", reject);
+    discordClient.login(DISCORD_BOT_TOKEN);
   });
-  await discordClient.login(DISCORD_BOT_TOKEN);
+  log ("Discord client now logged in.");
 
   // *******************************************
 
@@ -78,10 +77,11 @@ export default async function () {
 
   log("Configuring speak function...");
   const speak = async (message: string, voice?: string) => {
+    try {
       const [{ audioContent }] = await ttsClient.synthesizeSpeech({
         input: { text: message },
         voice: { name: voice, languageCode: "en-US" },
-        audioConfig: { audioEncoding: "LINEAR16" },
+        audioConfig: { audioEncoding: "MP3" },
       });
       const ttsStream = new stream.Readable({ read: () => {} });
       ttsStream.push(audioContent);
@@ -90,30 +90,55 @@ export default async function () {
       player.play(resource);
       await entersState(player, AudioPlayerStatus.Playing, 5e3);
       connection.subscribe(player);
+      await entersState(player, AudioPlayerStatus.Idle, 60e3);
+    } catch (err: any) {
+      console.log(err.message ?? err);
+    }
   }
 
   // *******************************************
 
-  log("Configuring Twitch client...");
+  log("Configuring TTS queue function...");
   let currentUser: string | undefined;
   let timeoutId: number | undefined;
+  const userVoice: Record<string, string> = {
+    "ChevCast": "en-US-Wavenet-J",
+    "Codemanis": "en-AU-Standard-B",
+    "Ember_Stone": "en-US-Wavenet-H",
+    "harlequindollface": "en-US-Wavenet-F",
+    "noobpieces": "en-IN-Wavenet-D"
+  };
+  const ttsQueue: { username: string, message: string }[] = [];
+  let queueInProgress = false;
+  const processQueue = async () => {
+    if (queueInProgress) return;
+    queueInProgress = true;
+    while (ttsQueue.length > 0) {
+      const { username, message } = ttsQueue.pop()!;
+      let voice = userVoice[username] ?? availableVoices.pop();
+      const ttsMessage = username === currentUser || message.startsWith("ALEXA") ? message : `${enunciateUsername(username)} says ${message}`;
+      await speak(ttsMessage, voice);
+      currentUser = username;
+      if (timeoutId) clearTimeout(timeoutId);
+      setTimeout(() => currentUser = undefined, 45e3);
+    }
+    queueInProgress = false;
+  };
+  log("TTS queue function created.");
+
+  // *******************************************
+
+  log("Configuring Twitch client...");
   const twitchClient = new TwitchClient({
-    channels: CHANNELS.split(",")
+    channels: [...twitchChannels]
   });
   twitchClient.on("message", async (channel, tags, message, self) => {
     try {
       const username = tags["display-name"];
       log(`Twitch message: ${channel}:${username}: ${message}`);
       if (typeof username !== "string") return;
-      let voice = userVoice[username];
-      if (!voice) {
-        voice = userVoice[username] = englishVoices![Math.floor(Math.random()*englishVoices!.length)];
-      }
-      const ttsMessage = username === currentUser ? message : `${username} says ${message}`;
-      await speak(ttsMessage, voice);
-      currentUser = username;
-      if (timeoutId) clearTimeout(timeoutId);
-      setTimeout(() => currentUser = undefined, 45e3);
+      ttsQueue.unshift({ username, message });
+      processQueue();
     } catch (err: any) {
       console.log(err.message ?? err);
     }
@@ -123,11 +148,54 @@ export default async function () {
 
   // *******************************************
 
-  const readyMsg = `Chevbot is now listening to Twitch chat for ${CHANNELS.split(",").map(cleanUsername).join(", ")}!`;
-  console.log(readyMsg);
-  await speak(readyMsg);
+  const readyMsg = (channels: any) => `Chevbot is now listening to Twitch chat for ${channels}!`;
+  log(readyMsg(twitchChannels.join(", ")));
+  await speak(readyMsg(twitchChannels.map(cleanUsername).join(", ")));
+  
+  // const testVoices = englishVoices!;
+  // await speak(`There are ${testVoices.length} voices available.`);
+  // await keypress();
+  // for (let index=0; index < testVoices.length; index++) {
+  //   const voice = testVoices[index];
+  //   const msg = `Voice ${index}: Hi this is Codeman`;
+  //   console.log(`Voice ${index}: ${voice}`);
+  //   await speak(msg, voice);
+  //   await keypress();
+  // }
 };
+
+async function keypress() {
+  process.stdin.setRawMode(true);
+  return new Promise<void>(resolve => process.stdin.once('data', () => {
+    process.stdin.setRawMode(false);
+    resolve();
+  }));
+}
 
 function cleanUsername(username: string) {
   return username.replace(/(\d+|[_-])/g, " ");
+}
+
+function enunciateUsername(username: string) {
+  username = cleanUsername(username);
+  if (username.includes(" ")) username = username.slice(0, username.indexOf(" "));
+  return username;
+}
+
+function shuffle(array: any[]) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
 }
--- server/server.ts
+++ server/server.ts
@@ -7,33 +7,50 @@ import next from "next";
 import config from "config";
 import { initDb } from "db";
 import debug from "debug";
-import chatbot from "./chatbot";
+import createChatbot from "./chatbot";
 
 const log = debug("CHEVCASTTV:SERVER");
 
-const { APP_URL, NODE_ENV } = config;
-
+const {
+  APP_URL,
+  NODE_ENV,
+  DISCORD_CHEVCAST_GUILD_ID,
+  DISCORD_CHEVCAST_LIVESTREAM_TEXT_CHANNEL_ID,
+  DISCORD_CHEVCAST_LIVESTREAM_VOICE_CHANNEL_ID,
+  DISCORD_WATERCOOLER_GUILD_ID,
+  DISCORD_WATERCOOLER_LIVESTREAM_VOICE_CHANNEL_ID,
+  TWITCH_CHANNELS
+} = config;
 
 (async () => {
 
   log("Initializing database...");
   await initDb();
 
-  // log("Preparing nextjs app...");
-  // const app = next({ dev: NODE_ENV !== "production"});
-  // const handle = app.getRequestHandler();
-  // await app.prepare();
+  log("Preparing nextjs app...");
+  const app = next({ dev: NODE_ENV !== "production"});
+  const handle = app.getRequestHandler();
+  await app.prepare();
 
-  // log("Starting http server...");
-  // const server = createServer((req, res) => {
-  //   const parsedUrl = parse(req.url ?? "", true);
-  //   handle(req, res, parsedUrl);
-  // });
-  // await server.listen(3000);
-  // console.log(`> ChevCastTV is running at ${APP_URL} in ${NODE_ENV} mode.`);
+  log("Starting http server...");
+  const server = createServer((req, res) => {
+    const parsedUrl = parse(req.url ?? "", true);
+    handle(req, res, parsedUrl);
+  });
+  await server.listen(3000);
+  console.log(`> ChevCastTV is running at ${APP_URL} in ${NODE_ENV} mode.`);
 
   log("Starting chatbot...");
-  await chatbot();
+  await createChatbot(
+    TWITCH_CHANNELS.split(","),
+    DISCORD_WATERCOOLER_GUILD_ID,
+    DISCORD_WATERCOOLER_LIVESTREAM_VOICE_CHANNEL_ID
+  );
+  await createChatbot(
+    TWITCH_CHANNELS.split(","),
+    DISCORD_CHEVCAST_GUILD_ID,
+    DISCORD_CHEVCAST_LIVESTREAM_VOICE_CHANNEL_ID
+  );
   console.log("> Chatbot active.");
 
   // log("Initializing Chevbot...");
