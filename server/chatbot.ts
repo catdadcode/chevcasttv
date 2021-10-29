7d74565bb58c9b55850a147ff4885f46bfcdc663
Alex Ford <alex.ford@codetunnel.com>
2021-10-29 12:25:51 -0600

    Bot ignore messages that start with exclamation

--- server/chatbot.ts
+++ server/chatbot.ts
@@ -106,6 +106,7 @@ export default class Chatbot {
   queueMessage(username: string, message: string) {
     const blackList = ["streamelements", "soundalerts"];
     if (blackList.includes(username.toLowerCase())) return;
+    if (message.startsWith("!")) return;
     const urlRegex = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
     message = message.replace(urlRegex, "");
     this.ttsQueue.unshift({ username, message });
